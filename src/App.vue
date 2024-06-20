89de293151695882446cb24da1ece2cc60929b11
Ian Feng <ian@mygreenbutler.com>
2024-06-20 23:57:00 +1000

    Calendar very basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,38 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="page-container">
+    <the-header></the-header>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+  import TheHeader from "@/components/TheHeader.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+  export default {
+    components: {
+      TheHeader,
+    },
+  };
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+<style>
+  * {
+    box-sizing: border-box;
+  }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+  html {
+    font-family: "Roboto", sans-serif;
+  }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+  body, html {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .page-container {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
+    height: 100%;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/page/calendar/CalendarPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="column">Column 1</div>
+        <div class="column">Column 2</div>
+        <div class="column">Column 3</div>
+        <div class="column">Column 4</div>
+        <div class="column">Column 5</div>
+        <div class="column">Column 6</div>
+        <div class="column">Column 7</div>
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        display: grid;
+        grid-template-columns: repeat(7, 1fr);
+        grid-gap: 0px;
+        flex: 1 1 auto;
+    }
+
+    .column {
+        background-color: #f1f1f1;
+        padding: 20px;
+        text-align: center;
+        position: relative;
+    }
+
+    .column::after {
+        content: "";
+        position: absolute;
+        right: 0; /* Align to the right edge */
+        top: 10%; /* Start at 10% from the top to vertically center */
+        width: 1px; /* Width of the border */
+        height: 80%; /* Height of the border */
+        background-color: #ccc; /* Color of the border */
+    }
+</style>
